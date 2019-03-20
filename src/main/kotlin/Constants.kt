b3957a4baebf084bba357d510f0c4a3bac1d4f86
antonsarmatin <antonsarmatin@googlemail.com>
2019-03-20 11:23:18 +0300

    object constants

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,31 @@
+object Constants {
+    val ROBOT_MIN_RADIUS = 1
+    val ROBOT_MAX_RADIUS = 1.05
+    val ROBOT_MAX_JUMP_SPEED = 15
+    val ROBOT_ACCELERATION = 100
+    val ROBOT_NITRO_ACCELERATION = 30
+    val ROBOT_MAX_GROUND_SPEED = 30
+    val ROBOT_ARENA_E = 0
+    val ROBOT_RADIUS = 1
+    val ROBOT_MASS = 2
+    val TICKS_PER_SECOND = 60
+    val MICROTICKS_PER_TICK = 100
+    val RESET_TICKS = 2 * TICKS_PER_SECOND
+    val BALL_ARENA_E = 0.7
+    val BALL_RADIUS = 2
+    val BALL_MASS = 1
+    val MIN_HIT_E = 0.4
+    val MAX_HIT_E = 0.5
+    val MAX_ENTITY_SPEED = 100
+    val MAX_NITRO_AMOUNT = 100
+    val START_NITRO_AMOUNT = 50
+    val NITRO_POINT_VELOCITY_CHANGE = 0.6
+    val NITRO_PACK_X = 20
+    val NITRO_PACK_Y = 1
+    val NITRO_PACK_Z = 30
+    val NITRO_PACK_RADIUS = 0.5
+    val NITRO_PACK_AMOUNT = 100
+    val NITRO_PACK_RESPAWN_TICKS = 10 * TICKS_PER_SECOND
+    val GRAVITY = 30
+    val EPS = 0.0000001
+}
